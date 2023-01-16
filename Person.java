f4564980ab66ab34258291c6f2f487393197d269
Sam Phipps <samuel.v.phipps@gmail.com>
2023-01-16 11:47:59 -0600

    whoIsThis method

--- Person.java
+++ Person.java
@@ -1,19 +1,25 @@
 public class Person {
-        String name;
-        boolean isAwesome;
-        int age;
+    String name;
+    boolean isAwesome;
+    int age;
 
-    public Person(String personName, boolean personIsAwesome, int personAge){
-            name = personName;
-            isAwesome = personIsAwesome;
-            age = personAge;
-        }
-        public static void main(String [] args) {
-            //prints to console. Must compile first
-            //System.out.print("") will print but wont create a new line
+    public Person(String personName, boolean personIsAwesome, int personAge) {
+        name = personName;
+        isAwesome = personIsAwesome;
+        age = personAge;
+    }
+
+    public void whoIsThis() {
+        System.out.println(name + " is " + age + " years old.");
+    }
+
+    public static void main(String[] args) {
+        //prints to console. Must compile first
+        //System.out.print("") will print but wont create a new line
 //        System.out.println("Hello World");
-            Person boss = new Person("Jeremy", false, 33);
-            System.out.println("My boss is " + boss.name);
-        }
+        Person boss = new Person("Jeremy", false, 33);
+        System.out.println("My boss is " + boss.name);
+        boss.whoIsThis();
+    }
 
 }
