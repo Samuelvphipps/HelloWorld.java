920aab4f25a6726c32ae4ef894f1d6fc6466b265
Sam Phipps <samuel.v.phipps@gmail.com>
2023-01-16 09:11:40 -0600

    comments on format

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,5 +1,9 @@
+//HelloWorld class must match file name
 public class HelloWorld {
+    //main method to run code
     public static void main(String [] args) {
+        //prints to console. Must compile first
+        //System.out.print("") will print but wont create a new line
         System.out.println("Hello World");
 
     }
