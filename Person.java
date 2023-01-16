baf2b276dc830eef9d328fa5b305982215683f38
Sam Phipps <samuel.v.phipps@gmail.com>
2023-01-16 10:49:50 -0600

    person object

---
+++ Person.java
@@ -0,0 +1,19 @@
+public class Person {
+        String name;
+        boolean isAwesome;
+        int age;
+
+    public Person(String personName, boolean personIsAwesome, int personAge){
+            name = personName;
+            isAwesome = personIsAwesome;
+            age = personAge;
+        }
+        public static void main(String [] args) {
+            //prints to console. Must compile first
+            //System.out.print("") will print but wont create a new line
+//        System.out.println("Hello World");
+            Person boss = new Person("Jeremy", false, 33);
+            System.out.println("My boss is " + boss.name);
+        }
+
+}
